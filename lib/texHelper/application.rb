d15290b987ce7db8d4c6f0d272e55a8ece2ab031
Paul Martensen <stu102532@mail.uni-kiel.de>
2015-10-03 23:04:55 +0200

    Cleaned up application according to rubocop

--- lib/texHelper/application.rb
+++ lib/texHelper/application.rb
@@ -3,6 +3,7 @@ require 'pathname'
 require 'ruby-progressbar'
 
 module TexHelper
+  ## Main Application class. Inheriting from Thor.
   class Application < Thor
     include Thor::Actions
     include TexHelper::Utility
@@ -13,37 +14,33 @@ module TexHelper
     end
 
     # TODO
-    desc 'include PARTIAL MASTER', 'Includes one or many Partial tex File at the End of a Master file. NOT WORKING YET.'
+    desc 'include PARTIAL MASTER', 'Includes one or many Partial tex
+    File at the End of a Master file. NOT WORKING YET.'
     def include(partial, _master)
       puts find_EOD partial
     end
 
-    desc 'cleanCompile FILE', 'cc for short.Compiles a Tex-File cleanly by Extracting the auxilary Files into a different seperate directory.'
-    def cleanCompile(file)
+    desc 'cleanCompile FILE', 'cc for short.Compiles a Tex-File cleanly by
+    Extracting the auxilary Files into a different seperate directory.'
+    def clean_compile(file)
       pb = ProgressBar.create
       file = Pathname.new(file)
-
       Dir.chdir(file.parent)
-
       aux_dir = file.basename('.tex').to_s + '_aux'
       puts aux_dir
       10.times { pb.increment }
-
       system "mkdir -p #{aux_dir}"
       10.times { pb.increment }
-
       puts file.basename
       2.times do
-        system "pdflatex -interaction=batchmode -output-directory=#{aux_dir} #{file.basename}"
+        system "pdflatex -interaction=batchmode
+        -output-directory=#{aux_dir} #{file.basename}"
         40.times { pb.increment }
       end
-
       system "mv #{aux_dir}/#{file.basename('.tex').to_s + '.pdf'} #{Dir.pwd}"
     end
 
     register(Generate, 'create', 'create <snippet>', 'Create a tex-snippet.')
     map cc: :cleanCompile
-
-    private
   end
 end
